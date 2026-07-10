7697579a1bf8ceb9f718b7af06fe1c968218e478
rozari0 <rozario@disroot.org>
2026-07-10 23:29:44 +0600

    fix(products): Fix products pagination

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,11 +1,11 @@
 from rest_framework.generics import ListAPIView
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
 
 class ProductListView(ListAPIView):
-    pagination_class = PageNumberPagination
+    pagination_class = LimitOffsetPagination
     serializer_class = ProductSerializer
 
     def get_queryset(self):
