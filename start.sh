032434014448e72e42264f0f08c869736cf1aae8
rozari0 <rozario@disroot.org>
2026-07-16 19:40:10 +0600

    fix: Migrate first.

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-python manage.py seed_users && python manage.py seed_products
-python manage.py migrate && gunicorn --workers=2 -b 0.0.0.0:8000 config.wsgi
+python manage.py migrate
+python manage.py seed_users && python manage.py seed_product
+gunicorn --workers=2 -b 0.0.0.0:8000 config.wsgi
